05ab9f572d3d61a37d2d469db7b07d7384be9103
Joseph El-Khouri <joseph.elkhouri@gmail.com>
2016-01-01 09:25:06 -0500

    init

---
+++ SymbolMapMerger/src/Main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import info.savestate.symbolmapmerger.*;
+
+/**
+ *
+ * @author Joseph El-Khouri
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        SymbolMapMerger smm = new SymbolMapMerger();
+    }
+    
+}
